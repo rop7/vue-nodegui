7c212654c3b8a669aba0ac46d06fb5f9e114981b
@ropsoft <rop7@null.net>
2025-07-18 09:55:23 -0300

    fix

--- src/vueLoader.ts
+++ src/vueLoader.ts
@@ -5,12 +5,18 @@ const V_MODEL_TEXT = Symbol('vModelText');
 const V_MODEL_SLIDER = Symbol('vModelSlider');
 const V_MODEL_SPINBOX = Symbol('vModelSpinBox');
 const V_MODEL_COMBOBOX = Symbol('vModelComboBox');
+const V_MODEL_ARRAY = Symbol('vModelArray')
+const V_MODEL_OBJECT = Symbol('vModelObject')
+const V_MODEL_BOOLEAN = Symbol('vModelBoolean')
 
 registerRuntimeHelpers({
   [V_MODEL_TEXT]: 'vModelText',
   [V_MODEL_SLIDER]: 'vModelSlider',
   [V_MODEL_SPINBOX]: 'vModelSpinBox',
   [V_MODEL_COMBOBOX]: 'vModelComboBox',
+  [V_MODEL_ARRAY]: 'vModelArray',
+  [V_MODEL_OBJECT]: 'vModelObject',
+  [V_MODEL_BOOLEAN]: 'vModelBoolean'
 });
 
 type CompilerOptions = {
@@ -21,14 +27,23 @@ type CompilerOptions = {
 }
 
 export const compilerOptions: CompilerOptions = {
+
   isNativeTag: isNativeWidget,
+
   directiveTransforms: {
+
     model: (dir, node, context) => {
+
       const baseResult = baseTransform(dir, node, context);
       const { tag } = node;
+
       let directiveToUse;
+
       switch (tag) {
         case 'vn-line-edit':
+        case 'QLineEdit':
+        case 'QInput':
+        case 'QText':
           directiveToUse = V_MODEL_TEXT;
           break;
         case 'vn-slider':
@@ -41,14 +56,25 @@ export const compilerOptions: CompilerOptions = {
         case 'vn-combobox':
           directiveToUse = V_MODEL_COMBOBOX;
           break;
+        case 'QCheck':
+          directiveToUse = V_MODEL_BOOLEAN;
+          break;
+        case 'QList':
+          directiveToUse = V_MODEL_ARRAY;
+          break;
+        case 'QData':
+        case 'QFilter':
+        case 'QToggle':
+          directiveToUse = V_MODEL_OBJECT;
+          break;
         default:
           throw new Error(`cannot use v-model on tag: ${tag}`);
       }
+
       baseResult.needRuntime = context.helper(directiveToUse as symbol);
-      // console.log('dir', dir);
-      // console.log('node', node);
-      // console.log('context', context);
+
       return baseResult;
+      
     },
   },
 };
